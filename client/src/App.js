c2a870ed4e2eaf4aca551877a6e5d88e36057657
Kevin Haube <thekevinhaube@gmail.com>
2019-12-03 21:54:55 -0500

    Heroku build

---
+++ client/src/App.js
@@ -0,0 +1,311 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+import { LinearProgress } from '@material-ui/core';
+import ForecastListItem from './Components/ForecastListItem'
+import LiveTemperature from './Components/LiveTemperature';
+import IconButton from '@material-ui/core/IconButton';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import SearchBar from './Components/SearchBar';
+import Panel from './Components/Panel';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackBarContentWrapper from './Components/SnackBarContentWrapper'
+
+
+const useStyles = makeStyles(theme => ({
+  theme: {
+    backgroundImage: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)',
+    color: '#ffffff'
+  },
+  card: {
+    maxWidth: '345',
+    textAlign: 'left'
+  },
+  whiteText: {
+    color: '#ffffff'
+  },
+  root: {
+    flexGrow: 1,
+    backgroundColor: '#f7f7f7',
+    height: '100vh',
+    width: '100vw'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  subtitle: {
+    marginBottom: '20px',
+  },
+  padded: {
+    padding: theme.spacing(3, 2)
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  },
+  paperTheme: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: '#ffffff',
+    backgroundImage: 'linear-gradient(135deg, #667eea 0%, #764ba2 100%)'
+  },
+  loader: {
+    display: 'block',
+    margin: 'auto'
+  }
+}));
+
+const ColorLinearProgress = withStyles({
+  colorPrimary: {
+    backgroundColor: '#667eea',
+  },
+  barColorPrimary: {
+    backgroundColor: '#764ba2',
+  },
+})(LinearProgress);
+
+const callApi = async () => {
+  
+}
+
+export default function App() {
+
+  const [appName, changeAppName] = useState('WeatherHub')
+  const [city, updateCity] = useState('Sykesville, MD')
+  const [forecast, updateForecast] = useState({})
+  const [lat, updateLat] = useState(36.9956066)
+  const [lng, updateLng] = useState(-91.0145714)
+  const [loaded, updateLoaded] = useState(false)
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  //SnackBar Open/Close
+  const [snackOpen, setSnackOpen] = useState(false);
+  const [snackVar, setSnackVar] = useState('error')
+  const [snackMsg, setSnackMsg] = useState('default message')
+
+  const classes = useStyles();
+  const open = Boolean(anchorEl);
+  const testText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor non ipsum et feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis imperdiet nisl sed metus accumsan commodo. Maecenas urna orci, placerat ut lobortis eu, facilisis nec augue. Etiam eu odio quis ex luctus imperdiet sed eget mi. Fusce volutpat vel libero nec dapibus. In hac habitasse platea dictumst."
+
+  // useEffect(() => {
+  //   updateLoaded(false)
+  //   axios.get(`https://api.darksky.net/forecast/09b2001e4b878941580a9e3460cb83e4/${lat},${lng}`)
+  //   .then(response => {
+  //     updateForecast(response.data)
+  //     console.log("Forecast: Got it!  ===  " + response.data)
+  //   })
+  //   .catch(error => {
+  //     console.log("Error getting Forecast: " + error)
+  //   })
+  // }, [lat, lng])
+
+  useEffect(() => {
+    updateLoaded(false)
+    axios.get(`/api/hello/${lat},${lng}`)
+      .then(res => {
+        updateForecast(res.data)
+      })
+      .catch(error => {
+        console.log("Oops... " + error)
+      })
+  }, [lat, lng])
+
+  useEffect(() => {
+    if (forecast.currently != undefined) {
+      updateLoaded(true)
+    } else {
+      console.log("Forecast is undefined.")
+    }
+  }, [forecast])
+
+  const searchCallback = search => {
+    if (!search) {
+      handleSnackOpen('error', 'Invalid search, please try again.')
+    } else {
+      axios.get(`https://public.opendatasoft.com/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=${search}`)
+      .then(response => {
+        var data = response.data
+
+        if (data.records[0] != undefined) {
+          updateCity(data.records[0].fields.city + ", " + data.records[0].fields.state)
+          updateLat(data.records[0].fields.latitude)
+          updateLng(data.records[0].fields.longitude)
+        } else {
+          handleSnackOpen('error', `Zip-code not found: ${search}`);
+        }
+      })
+    }
+  }
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleSnackOpen = (variant, message) => {
+    setSnackVar(variant)
+    setSnackMsg(message)
+    setSnackOpen(true)
+  }
+  const handleSnackClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackOpen(false);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleMenuClick = (address) => {
+    handleClose()
+  };
+
+  if (!loaded) {
+    return(
+      <div className={classes.root}>
+      <AppBar className={classes.theme} position="static">
+          <Toolbar>
+            <Typography className={classes.title} variant="h6">
+              {appName}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <ColorLinearProgress />
+      </div>
+    )
+  } else {
+    return (
+      <div className={classes.root}>
+  
+      <AppBar className={classes.theme} position="static">
+          <Toolbar>
+            <Typography className={classes.title} variant="h6">
+              {appName}
+            </Typography>
+            <SearchBar searchListener={searchCallback} />
+            <IconButton
+              edge="end"
+              aria-label="display more actions"
+              aria-haspopup="true"
+              onClick={handleMenuClick}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Metric</MenuItem>
+              <MenuItem onClick={handleClose}>Imperial</MenuItem>
+            </Menu>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.padded} maxWidth="lg">
+          <Grid container direction='row' spacing={2}>
+
+            <Grid item xs={12} md={4}>
+              <Grid container direction='column' spacing={2}>
+  
+                <Grid item>
+                  <Paper className={classes.paperTheme}>
+                    <LiveTemperature currently={forecast.currently} />
+                  </Paper>
+                </Grid>
+  
+                <Grid item>
+                  <Paper className={classes.paper}>
+                    <Grid container direction='column' alignItems='flex-start'>
+                      <Grid item>
+                        <Typography variant='h5'>
+                          7 Day Forecast
+                        </Typography>
+                      </Grid>
+                      <Grid item>
+                        <Typography variant='body1' className={classes.subtitle}>
+                          {forecast.daily.summary}
+                        </Typography>
+                      </Grid>
+                      {
+                        forecast.daily != undefined ? 
+                        forecast.daily.data.map((day) => 
+                        <ForecastListItem key={day.time} day={day} />)
+                        : null
+                      }
+                    </Grid>
+                  </Paper>
+                </Grid>
+  
+              </Grid>
+            </Grid>
+            <Grid item md={8}>
+
+              <Grid container direction='column' spacing={2}>
+
+                <Grid item>
+                  <Panel data={forecast} icon='loc' variant='current-forecast-details' title={city} />
+                </Grid>
+
+                <Grid item>
+                  <Grid container direction='row' spacing={2}>
+
+                    <Grid item md={6}>
+                      <Panel data={testText} icon='help' variant='text' title="Information" />
+                    </Grid>
+
+                    <Grid item md={6}>
+                      <Panel data={testText} icon='help' variant='text' title="Information" />
+                    </Grid>
+
+                  </Grid>
+                </Grid>
+
+              </Grid>
+              
+            </Grid>
+
+          </Grid>
+        </Container>
+
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={snackOpen}
+          autoHideDuration={3000}
+          onClose={handleSnackClose}
+        >
+          <SnackBarContentWrapper
+            onClose={handleSnackClose}
+            variant={snackVar}
+            message={snackMsg}
+          />
+        </Snackbar>
+        
+      </div>
+    );
+  }
+}
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const axios = require('axios')
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello/:lat,:lng', (req, res) => {
+  axios.get(`https://api.darksky.net/forecast/09b2001e4b878941580a9e3460cb83e4/${req.params.lat},${req.params.lng}`)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(error => {
+      res.send(error)
+    })
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+    
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
